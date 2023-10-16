ff3d499482171f35d6639457eb7c03d5ef3bec9b
zhengxixuan <930042468@qq.com>
2023-10-16 16:41:30 +0800

    算法

--- docs/frontend-advanced/algorithm/back-trace.md
+++ docs/frontend-advanced/algorithm/back-trace.md
@@ -377,3 +377,56 @@ var permute = function (nums) {
     return result
 };
 ```
+
+### N 皇后
+
+链接：<https://leetcode.cn/problems/n-queens/description/>
+
+代码示例：
+
+```js
+var transform = function (board) {
+  return board.map(item => {
+    return item.join('')
+  })
+}
+var isValid = function (row, col, board, n) {
+  // 纵向
+  for (let i = 0; i < board.length; i++) {
+    if (board[i][col] === 'Q') return false
+  }
+
+  // 左上
+  for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+    if (board[i][j] === 'Q') return false
+  }
+
+  // 右上
+  for (let i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+    if (board[i][j] === 'Q') return false
+  }
+  return true
+}
+
+var solveNQueens = function (n) {
+  const result = []
+  const board = new Array(n).fill([]).map(() => new Array(n).fill('.'))
+
+  const backTrace = (row, board) => {
+    if (row === n) {
+      result.push(transform(board))
+      return
+    }
+    for (let col = 0; col < n; col++) {
+      if (isValid(row, col, board, n)) {
+        board[row][col] = 'Q'
+        backTrace(row + 1, board)
+        board[row][col] = '.'
+      }
+    }
+  }
+
+  backTrace(0, board)
+  return result
+}
+```
--- test.js
+++ test.js
@@ -1,17 +1,43 @@
-var permute = function (nums) {
+var transform = function (board) {
+  return board.map(item => {
+    return item.join('')
+  })
+}
+var isValid = function (row, col, board, n) {
+  // 纵向
+  for (let i = 0; i < board.length; i++) {
+    if (board[i][col] === 'Q') return false
+  }
+
+  // 左上
+  for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+    if (board[i][j] === 'Q') return false
+  }
+
+  // 右上
+  for (let i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+    if (board[i][j] === 'Q') return false
+  }
+  return true
+}
+
+var solveNQueens = function (board) {
   const result = []
-  const path = []
-  const backTrace = () => {
-    if (path.length === nums.length) {
-      return result.push([...path])
+
+  const backTrace = (row, board) => {
+    if (row === n) {
+      result.push(transform(board))
+      return
     }
-    for (let i = 0; i < nums.length; i++) {
-      if (path.includes(nums[i])) continue
-      path.push(nums[i])
-      backTrace()
-      path.pop()
+    for (let col = 0; col < n; col++) {
+      if (isValid(row, col, board, n)) {
+        board[row][col] = 'Q'
+        backTrace(row + 1, board)
+        board[row][col] = '.'
+      }
     }
   }
-  backTrace()
+
+  backTrace(0, board)
   return result
 }
